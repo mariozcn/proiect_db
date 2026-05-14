d39c7840c5dacf529f9c8bdeedf2a8ac72d01716
Rusu Mario-Antonio <rusu.io.mario@student.utcluj.ro>
2026-05-14 11:15:41 +0300

    initial commit

---
+++ src/main/java/com/example/proiect_db/ProiectDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiect_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proiect_db/ProiectDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiect_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
